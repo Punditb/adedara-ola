36ce4ef100095dd0f65dcf259762609420c77ccd
Punditb <emmanuelbright197@gmail.com>
2026-07-01 12:15:30 +0100

    Clean repository ignored files

--- .output/server/_ssr/project-mixed-Coi09pei.mjs
+++
@@ -1,8 +0,0 @@
-const residentialImg = "/assets/project-residential-tPYWyo1a.jpg";
-const commercialImg = "/assets/project-commercial-mUi2RqxX.jpg";
-const mixedImg = "/assets/project-mixed-DwNuCAcy.jpg";
-export {
-  commercialImg as c,
-  mixedImg as m,
-  residentialImg as r
-};
